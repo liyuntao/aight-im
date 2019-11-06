481f36db4a2ce8cf30c3696abdf8fe68ddf935de
liyuntao <liyuntao58607@gmail.com>
2019-11-06 17:01:42 +0800

    add type_id enum

--- aight-proto/src/types.rs
+++ aight-proto/src/types.rs
@@ -2,21 +2,27 @@ use bytes::BytesMut;
 use prost::Message;
 include!(concat!(env!("OUT_DIR"), "/aight_proto.msg_types.rs"));
 
+pub enum TypeID {
+    Login = 1,
+    EchoRequest,
+    SendRequest,
+}
+
 pub fn create_login(id: String) -> RawTcpMessage {
-    to_raw(1, Login { id })
+    to_raw(TypeID::Login as i32, Login { id })
 }
 
 pub fn create_echo(body: String) -> RawTcpMessage {
-    to_raw(2, EchoRequest { body })
+    to_raw(TypeID::EchoRequest as i32, EchoRequest { body })
 }
 
 pub fn create_send(to_id: String, body: String) -> RawTcpMessage {
-    to_raw(3, MsgSendRequest { to_id, body })
+    to_raw(TypeID::SendRequest as i32, MsgSendRequest { to_id, body })
 }
 
 fn to_raw<T: Message>(type_id: i32, msg: T) -> RawTcpMessage {
     let mut buffer = BytesMut::with_capacity(msg.encoded_len());
-    msg.encode(&mut buffer);
+    msg.encode(&mut buffer).expect("unreachable");
     RawTcpMessage {
         type_id,
         body: buffer.to_vec(),
